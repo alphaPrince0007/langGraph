5a769274a85177444cffd1fb22bf88d86954d16f
Prince Sharma <alphaprince0093@gmail.com>
2026-03-22 15:20:58 +0530

    creating 2 routes

--- Conditional_Agent.py
+++ Conditional_Agent.py
@@ -4,64 +4,114 @@ from requests import get
 
 class AgentState(TypedDict):
     number1: int
-    operation: str
     number2: int
+    operation1: str
+    number3: int
+    number4: int
+    operation2: str
     finalNumber: int
+    finalNumber2: int
 
-def adder(state: AgentState) -> AgentState:
+def adder1(state: AgentState) -> AgentState:
     """This node adds the 2 numbers"""
     
     state["finalNumber"] = state["number1"] + state["number2"]
     return state
 
-def subtractor(state: AgentState) -> AgentState:
+def subtractor1(state: AgentState) -> AgentState:
     """This node substracts the 2 numbers"""
 
     state["finalNumber"] = state["number1"] - state["number2"]
     return state
 
-def decide_next_node(state: AgentState) -> AgentState:
+def adder2(state: AgentState) -> AgentState:
+    """This node adds the 2 numbers"""
+    
+    state["finalNumber2"] = state["finalNumber"] + state["number3"] + state["number4"]
+    return state
+
+def subtractor2(state:AgentState) -> AgentState:
+    """This node subtracts the 2 numbers"""
+    state["finalNumber2"] = state["finalNumber"] - state["number3"] - state["number4"]
+    return state
+
+def decide_layer_1(state: AgentState) -> AgentState:
     """This node will select the next node of the graph"""
 
-    if state["operation"] == "+":
+    if state["operation1"] == "+":
         return "addition_operation"
 
-    elif state["operation"] == "-":
+    elif state["operation1"] == "-":
         return "subtraction_operation"
+    else:
+        raise ValueError("Invalid operation")
+    
+
+def decide_layer_2(state: AgentState) -> AgentState:
+    """This node will select the next node of the graph"""
+
+    if state["operation2"] == "+":
+        return "addition_operation"
+
+    elif state["operation2"] == "-":
+        return "subtraction_operation"
+    else:
+        raise ValueError("Invalid operation")
+
     
 
 
 graph = StateGraph(AgentState)
 
-graph.add_node("add_node", adder)
-graph.add_node("subtract_node", subtractor)
-graph.add_node("router", lambda state:state) #passthrough function
+graph.add_node("add_node1", adder1)
+graph.add_node("subtract_node1", subtractor1)
+graph.add_node("add_node2", adder2)
+graph.add_node("subtract_node2", subtractor2)
 
-graph.add_edge(START, "router")
+graph.add_node("router1", lambda state:state) #passthrough function
+graph.add_node("router2", lambda state:state) #passthrough function
+
+graph.add_edge(START, "router1")
 
 graph.add_conditional_edges(
-    "router",
-    decide_next_node,
+    "router1",
+    decide_layer_1,
 
     {
         #Edge: Node
-        "addition_operation": "add_node",
-        "subtraction_operation": "subtract_node"
+        "addition_operation": "add_node1",
+        "subtraction_operation": "subtract_node1"
     }
 
 )
 
-graph.add_edge("add_node", END)
-graph.add_edge("subtract_node", END)
+graph.add_edge("add_node1", "router2")
+graph.add_edge("subtract_node1", "router2")
+
+graph.add_conditional_edges(
+    "router2",
+    decide_layer_2,
+    {
+        #Edge: Node
+        "addition_operation": "add_node2",
+        "subtraction_operation": "subtract_node2",
+    }
+)
+
+graph.add_edge("add_node2", END)
+graph.add_edge("subtract_node2", END)
 
 app = graph.compile()
 
-# graph_png = app.get_graph().draw_mermaid_png()
-# with open("graph2.png", "wb") as file:
-#     file.write(graph_png)
+graph_png = app.get_graph().draw_mermaid_png()
+with open("graph2.png", "wb") as file:
+    file.write(graph_png)
 
 
+initial_state = {
+    "number1": 10, "operation1": "-", "number2": 5,
+    "number3": 2, "operation2": "+", "number4": 3
+}
 
-initial_state_1 = AgentState(number1=7, operation="-", number2=5)
-print(app.invoke(initial_state_1))
+print(app.invoke(initial_state))
 
